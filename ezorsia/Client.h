9c545a353c9f91ca871a88a056a89af450c82a95
izarooni <coalasonly@gmail.com>
2019-11-27 15:06:43 -0700

    Reverting to default cipher key

--- ezorsia/Client.h
+++ ezorsia/Client.h
@@ -5,7 +5,7 @@ public:
 	static void EnableNewIGCipher();
 	static void UpdateResolution();
 	static void UpdateLogin();
-	static const int m_nIGCipherHash = 0x2D2511FF;
+	static const int m_nIGCipherHash = 0XC65053F2;
 	static const int m_nGameHeight = 768;
 	static const int m_nGameWidth = 1024;
 };
